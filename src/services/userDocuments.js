242294827e46578c58a2e02da9631e7f0f00e176
Nazarhussain12 <nazarliaqat123@gmail.com>
2026-01-10 21:17:24 +0500

    updated userddocs api

--- src/services/userDocuments.js
+++ src/services/userDocuments.js
@@ -1,9 +1,7 @@
-import axios from 'axios';
-
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5050/api';
+import api from './api';
 
 export const userDocumentsService = {
-    getUserDocuments: (userId) => axios.get(`${API_URL}/user-documents/user/${userId}`),
-    addDocument: (docData) => axios.post(`${API_URL}/user-documents`, docData),
-    deleteDocument: (id) => axios.delete(`${API_URL}/user-documents/${id}`)
+    getUserDocuments: (userId) => api.get(`/api/user-documents/user/${userId}`),
+    addDocument: (docData) => api.post('/api/user-documents', docData),
+    deleteDocument: (id) => api.delete(`/api/user-documents/${id}`)
 };
